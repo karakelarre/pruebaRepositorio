8ec220ef5a360b0517c8e46d51595e76b075a4f2
alu12v <alu12v@desarrolloweb.local>
2024-02-01 16:46:48 +0100

    Inicio del proyecto

---
+++ src/paqueteRepositorio/Repositorio.java
@@ -0,0 +1,19 @@
+
+package paqueteRepositorio;
+
+/**
+ *
+ * @author alu12v
+ */
+public class Repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Primer repositorio en Netbeans");
+        
+    }
+    
+}
